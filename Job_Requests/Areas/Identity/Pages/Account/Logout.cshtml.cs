1ef7cd2e35f952ae4a31d6e35147cf78e7e5e6ce
MichaelRichardSRodriguez <LycaNGithuB0815>
2025-04-03 13:53:49 +0800

    1.) Enhance UI for AccessDenied, Log-out, and lockout pages 2.) Implement Logout Confirmation and Message by revising and using Log-out Razor page

--- Job_Requests/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Job_Requests/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -27,7 +27,9 @@ namespace Job_Requests.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
+
             _logger.LogInformation("User logged out.");
+
             if (returnUrl != null)
             {
                 return LocalRedirect(returnUrl);
